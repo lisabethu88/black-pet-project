518e9e532e25a0ca7518053f6aa65c6ed78e9c68
Lisa Utsett <lisabethutsett@gmail.com>
2025-07-25 21:37:58 -0400

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "dist", // Vercel looks for this by default
+  },
+  server: {
+    host: true,
+  },
 });
